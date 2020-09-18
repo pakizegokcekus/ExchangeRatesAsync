922408cfb7b88656963ddf9594dbf9feaa6c8d5c
pakizekus <48360370+pakizekus@users.noreply.github.com>
2020-09-18 09:14:17 +0300

    first-commit

---
+++ ExchangeRatesAsync/FrmExchangeRate.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ExchangeRatesAsync
+{
+    partial class FrmExchangeRate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Code = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ForexBuying = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ForexSelling = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BankNoteBuying = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BankNoteSelling = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tmr_Refresh = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Code,
+            this.ForexBuying,
+            this.ForexSelling,
+            this.BankNoteBuying,
+            this.BankNoteSelling});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(604, 86);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Adı:";
+            this.Name.Width = 100;
+            // 
+            // Code
+            // 
+            this.Code.Text = "Kod:";
+            this.Code.Width = 100;
+            // 
+            // ForexBuying
+            // 
+            this.ForexBuying.Text = "Döviz Alış";
+            this.ForexBuying.Width = 100;
+            // 
+            // ForexSelling
+            // 
+            this.ForexSelling.Text = "Döviz Satış";
+            this.ForexSelling.Width = 100;
+            // 
+            // BankNoteBuying
+            // 
+            this.BankNoteBuying.Text = "Efektif Alış";
+            this.BankNoteBuying.Width = 100;
+            // 
+            // BankNoteSelling
+            // 
+            this.BankNoteSelling.Text = "Efektif Satış";
+            this.BankNoteSelling.Width = 100;
+            // 
+            // tmr_Refresh
+            // 
+            this.tmr_Refresh.Tick += new System.EventHandler(this.tmr_Refresh_Tick);
+            // 
+            // FrmExchangeRate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(627, 99);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+          
+            this.Load += new System.EventHandler(this.FrmExchangeRate_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Code;
+        private System.Windows.Forms.ColumnHeader ForexBuying;
+        private System.Windows.Forms.ColumnHeader ForexSelling;
+        private System.Windows.Forms.ColumnHeader BankNoteBuying;
+        private System.Windows.Forms.ColumnHeader BankNoteSelling;
+        private System.Windows.Forms.Timer tmr_Refresh;
+    }
+}
+
---
+++ ExchangeRatesAsync/FrmExchangeRate.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ExchangeRatesAsync
+{
+    public partial class FrmExchangeRate : Form
+    {
+        public FrmExchangeRate()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmExchangeRate_Load(object sender, EventArgs e)
+        {
+            TestReader();
+            RefreshCheck();
+        }
+        async Task TestReader()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = true;
+
+            string[] liste = new string[7];
+            int i = 0;
+            using (XmlReader reader = XmlReader.Create("http://www.tcmb.gov.tr/kurlar/today.xml", settings))
+            {
+                while (await reader.ReadAsync())
+                {
+
+
+                    switch (reader.NodeType)
+                    {
+
+                        case XmlNodeType.Text:
+                            liste[i] = reader.Value;
+                            await reader.GetValueAsync();
+                            if (i == 6)
+                            {
+
+                                if (liste[1].ToString() == "ABD DOLARI")
+                                {
+                                    ListViewItem item = new ListViewItem(liste[1].ToString());
+                                    item.SubItems.Add(liste[2].ToString());
+                                    item.SubItems.Add(liste[3].ToString());
+                                    item.SubItems.Add(liste[4].ToString());
+                                    item.SubItems.Add(liste[5].ToString());
+                                    item.SubItems.Add(liste[6].ToString());
+
+                                    i = -5;
+                                    listView1.Items.Add(item);
+                                }
+
+                                if (liste[1].ToString() == "EURO")
+                                {
+                                    ListViewItem item = new ListViewItem(liste[1].ToString());
+                                    item.SubItems.Add(liste[1].ToString());
+                                    item.SubItems.Add(liste[2].ToString());
+                                    item.SubItems.Add(liste[3].ToString());
+                                    item.SubItems.Add(liste[4].ToString());
+                                    item.SubItems.Add(liste[5].ToString());
+
+                                    i = -1;
+                                    listView1.Items.Add(item);
+                                }
+
+                                i = 0;
+                            }
+
+                            i++;
+
+                            break;
+                    }
+
+
+                }
+            }
+        }
+
+        private void tmr_Refresh_Tick(object sender, EventArgs e)
+        {
+            
+            TestReader();
+            listView1.Items.Clear();
+            // MessageBox.Show("Çalıştı");
+        }
+        public void RefreshCheck()
+        {
+            try
+            {
+                Timer tmr = new Timer();
+                tmr.Interval = (15 * 1 * 1000);//5 sec
+                tmr.Tick += new EventHandler(tmr_Refresh_Tick);
+                tmr.Start();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+    }
+}
